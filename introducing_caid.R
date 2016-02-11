266aab7d22ec336fa80b9fa73f001b6c56e1f237
RanaivosonHerimanitra <air_manitra@yahoo.fr>
2016-02-11 08:47:42 +0300

    to proportion

--- introducing_caid.R
+++ introducing_caid.R
@@ -9,10 +9,26 @@ caid=create_facies(caid)
 if ( input$Cluster_algo=="Total")
 {
   cat('calculating number of intervention in caid by code (date)...')
-  caid=caid[,sum(caid_value,na.rm = T),by="code"];setnames(caid,"V1","caid_value")
+  Nbsites_received=caid[,sum(caid_value,na.rm = T),by="code"]
+  setnames(Nbsites_received,"V1","numerateur")
+  Nbsites_actif=caid[is.na(caid_value)==F,length(unique(sites)),by="code"]
+  setnames(Nbsites_actif,"V1","denominateur")
+  caid= merge(Nbsites_received,Nbsites_actif,by.x="code",by.y="code",all.x=T)
+  caid[,mild_value:=numerateur/denominateur]
+  
+  
+  
+  
   cat('DONE\n')
 } else {
   cat('calculating number of intervention in mild by code (date) and by facies...')
-  caid=caid[,sum(caid_value,na.rm = T),by="code,facies"];setnames(caid,"V1","caid_value")
+  Nbsites_received=caid[,sum(caid_value,na.rm = T),by="code,facies"]
+  setnames(Nbsites_received,"V1","numerateur")
+  Nbsites_actif=caid[is.na(caid_value)==F,length(unique(sites)),by="code,facies"]
+  setnames(Nbsites_actif,"V1","denominateur")
+  caid= merge(Nbsites_received,Nbsites_actif,
+              by.x=c("code","facies"),
+              by.y=c("code","facies"),all.x=T)
+  caid[,caid_value:=numerateur/denominateur]
   cat('DONE\n')
 }
