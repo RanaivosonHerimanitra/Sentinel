0bc41fc0a3fb79bd570272f21bbe1a219a048447
RanaivosonHerimanitra <air_manitra@yahoo.fr>
2016-04-27 14:21:03 +0300

    fix reporting phrases on the main report

--- prep_narr_tdr.R
+++ prep_narr_tdr.R
@@ -5,13 +5,16 @@ mylength_tdr=0
 X=PaluConf_tdr[code==mycode[pos_j] & name==k,get("manque_tdr")]
 
 #get length of X,which corresponds to duration of alert (lack of RDT kit)
-mylength_tdr=length(X)
-# d=0
-# while ( all(X==1) & (pos_j+d)<=length(mycode) )
-# {
-#     mylength_tdr = mylength_tdr +1
-#     d=d+1
-#     X=PaluConf_tdr[code %in% mycode[pos_j:(pos_j+d)] & name==k,get("manque_tdr")]
-#     
-# }
+#annee_duration = PaluConf_tdr[code==mycode[pos_j],get("Annee")]
+#semaine_duration= PaluConf_tdr[code==mycode[pos_j],get("Semaine")]
+
+#mylength_tdr=nrow(PaluConf_tdr[name==k & Annee<=annee_duration & Semaine<=semaine_duration])
+d=0
+while ( length(X)>0 & (pos_j+d)<=length(mycode) )
+{
+    mylength_tdr = mylength_tdr +1
+    d=d+1
+    X=PaluConf_tdr[code %in% mycode[pos_j+d] & name==k,get("manque_tdr")]
+
+}
 
