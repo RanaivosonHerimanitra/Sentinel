21af18a0fff1cb9a5f6b4811e31211bf700e53ac
RanaivosonHerimanitra <air_manitra@yahoo.fr>
2016-05-11 12:21:40 +0300

    fix and update reporting process pipeline

--- generate_reports.sh
+++ generate_reports.sh
@@ -2,6 +2,6 @@
 Rscript "/media/herimanitra/Document/IPM_sentinelle/sentinel_hrmntr 291115/Sentinel/initialize_data.R"
 Rscript "/media/herimanitra/Document/IPM_sentinelle/sentinel_hrmntr 291115/Sentinel/execute_report.R"
 cd "/media/herimanitra/Document/IPM_sentinelle/sentinel_hrmntr 291115/Sentinel/report"
-zip report.zip Diar_sentinelles.pdf ILI_sentinelles.pdf missing_sent.docx palu_autoch_sentinelles.pdf PFA_sentinelles.pdf report.pdf
+zip report.zip Diar_sentinelles.pdf ILI_sentinelles.pdf missing_sent.docx palu_autoch_sentinelles.pdf Palu_sentinelles.pdf PFA_sentinelles.pdf report.pdf
 Rscript "send_email_report.R"
 
