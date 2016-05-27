8906f8fea0697557d911c94c0ef364432a0e23f6
RanaivosonHerimanitra <air_manitra@yahoo.fr>
2016-05-27 11:37:36 +0300

    select obs. starting from this year(2016)

--- report/miss_sent_report_shiny.R
+++ report/miss_sent_report_shiny.R
@@ -5,7 +5,9 @@ require(data.table)
 missing_sent= fread("/media/herimanitra/Document/IPM_sentinelle/sentinel_hrmntr 291115/Sentinel/data/missing_sent.csv")
 missing_sent[,code:=paste0(Annee,"_",ifelse(nchar(Semaine)<2,paste0("0",Semaine),Semaine))]
 cat("DONE\n")
-
+cat("select only >=2016 obs...")
+missing_sent=missing_sent[Annee>=2016]
+cat("DONE\n")
 cat("crosstabulation of weeks and Sites...")
 missing_sent[,Centre:=capitalize(tolower(Centre))]
 X=table(missing_sent$code,missing_sent$Centre)
