4b32ce119a3ec43169a88e85dd27f64670605f89
RanaivosonHerimanitra <air_manitra@yahoo.fr>
2016-06-09 16:05:41 +0300

    update main report to handle new sites and their impact on missing db alert (finalize sites to be reported)

--- manque_bd_narration.R
+++ manque_bd_narration.R
@@ -5,8 +5,12 @@ k_code=sentinel_latlong[name==k,get("sites")]
 Z1= PaluConf[code==mycode[pos_j],get(k_code)]
 Z2= Diarrh[code==mycode[pos_j],get(k_code)]
 
-if ( (k_code %in% c("ambl","abon","tanb","diri","dona","samp","snam","bela","mand","mnld","velo","vink","tsar","mora","tsim","miad","vola","tomp")) & which(mycode==j)>=which(mycode=="2015_29") ) {
-  cat("do nothing because ",k," included at ",j,"\n")
+# if belongs to these sites prior to 2015_29 then do nothing
+group1=any(k_code %in% c("ambl","abon","tanb","diri","dona","samp","snam","bela","mand","mnld","velo","vink","tsar","mora","tsim","miad","vola","tomp")) & which(mycode==j)>=which(mycode=="2015_29")
+# if belongs to "ANDRANOFITO" site prior to 2015_08 then do nothing
+group2= any(k_code %in% "fito" ) & which(mycode==j)>=which(mycode=="2015_09")
+if ( group1 | group2 ) {
+  cat("do nothing because data collection from ",k," site did not begin at ",j,"\n")
 } else {
   #count duration (length ) of lack of DB
   h=0
